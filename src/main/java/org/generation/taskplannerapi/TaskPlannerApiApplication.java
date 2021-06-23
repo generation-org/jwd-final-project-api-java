f5769b465d2ee89e6c8032a0c20e009fe3ec645d
sancarbar <sancarbar@gmail.com>
2021-06-23 08:45:49 -0500

    intial commit

---
+++ src/main/java/org/generation/taskplannerapi/TaskPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package org.generation.taskplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerApiApplication.class, args);
+	}
+
+}
